c82bda2bc7d07133e00c64965d208c0f0ddb28fc
proyectox5425 <antonioleonalva.1994@gmail.com>
2025-07-24 22:25:24 -0400

    Create pago_manual.py

---
+++ models/pago_manual.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, String, Float, DateTime
+from database import Base
+from datetime import datetime
+
+class PagoManual(Base):
+    __tablename__ = "pago_manual"
+
+    referencia = Column(String, primary_key=True, index=True)
+    telefono = Column(String)
+    banco = Column(String)
+    monto = Column(Float)
+    unidad = Column(String)
+    metodo = Column(String)
+    estado = Column(String, default="pendiente")
+    fecha = Column(DateTime, default=datetime.utcnow)
